0e26d0251caacd3d3fb469f09e2f960b9c58d6fe
Sophia Brandt <16630701+sophiabrandt@users.noreply.github.com>
2020-07-22 13:16:55 +0200

    📦 NEW: Add Routing

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_ddd/injection.dart';
 import 'package:flutter_ddd/presentation/core/app_widget.dart';
 import 'package:injectable/injectable.dart';
-import 'package:flutter_ddd/injection.dart';
 
 void main() {
   configureInjection(Environment.prod);
--- lib/presentation/core/app_widget.dart
+++ lib/presentation/core/app_widget.dart
@@ -1,20 +1,37 @@
+import 'package:auto_route/auto_route.dart';
+import 'package:flutter_ddd/presentation/routes/router.gr.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_ddd/application/auth/auth_bloc.dart';
+import 'package:flutter_ddd/injection.dart';
 import 'package:flutter_ddd/presentation/sign_in/sign_in_page.dart';
 
 class AppWidget extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(
+          create: (context) =>
+              getIt<AuthBloc>()..add(const AuthEvent.authCheckRequested()),
+        )
+      ],
+      child: MaterialApp(
         title: 'Notes',
         home: SignInPage(),
         debugShowCheckedModeBanner: false,
+        builder: ExtendedNavigator(router: Router()),
         theme: ThemeData.light().copyWith(
-            primaryColor: Colors.green[800],
-            accentColor: Colors.blueAccent,
-            inputDecorationTheme: InputDecorationTheme(
-                border: OutlineInputBorder(
+          primaryColor: Colors.green[800],
+          accentColor: Colors.blueAccent,
+          inputDecorationTheme: InputDecorationTheme(
+            border: OutlineInputBorder(
               borderRadius: BorderRadius.circular(8),
-            ))));
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
